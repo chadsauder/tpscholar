62524bbe205e08742ebf8166586847298e72bd34
Chad Sauder <chadsauder22@gmail.com>
2021-11-09 16:07:24 -0700

    first update

---
+++ tpscholar/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+<div id="app">
+  <div id="menu">
+    <div id="brand">
+      <router-link to="/">
+        <img src="/images/TP logo.png">
+      </router-link>
+    </div>
+    <div id="side">
+      <a href="https://github.com/chadsauder/tpscholar.git">
+        <img src="/images/GitHub_Logo.png">
+      </a>
+    </div>
+  </div>
+  <router-view />
+</div>
+</template>
+
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 50px 100px;
+
+}
+
+#menu {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 5px;
+  grid-template-areas: "none brand side";
+  margin-bottom: 50px;
+}
+
+#menu a {
+  color: #B84901;
+}
+
+#brand {
+  grid-area: brand;
+  display: flex;
+  justify-content: center;
+}
+
+#brand img {
+  height: 200px;
+}
+
+#side {
+  grid-area: side;
+  display: flex;
+  justify-content: flex-end;
+}
+
+#side img {
+  width: 50px;
+}
+
+.menu-item {
+  display: flex;
+  flex-direction: column;
+}
+
+.menu-item p {
+  margin: 0px;
+}
+
+.browse {
+  margin-right: 50px;
+}
+</style>
---
+++ tpscholar/src/mock-data.js
@@ -0,0 +1,11 @@
+let mock = [{
+    id: 1,
+    name: "The OG toilet paper scholarship",
+    value: "$200",
+    school: "Brigham Young University",
+    image: 'toiletpaper.jpeg',
+    about: "This scholarship comes from the Sauder family. The recipiant will recieve all the toilet paper while they are at school. Apply today for this scholarship!"
+  },
+]
+
+export default mock;
